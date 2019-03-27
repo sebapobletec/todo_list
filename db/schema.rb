585f0e012777f700b8df80ebac9a2f0e843988c6
Sebastian Poblete <sebapobletec@gmail.com>
2019-03-26 21:20:02 -0300

    Add boostrap, seed and todo index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_022611) do
+ActiveRecord::Schema.define(version: 2019_03_26_234800) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string "photo"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |i|
+  Todo.create(
+    title: "Actividad #{i+1}",
+    photo: "https://loremflickr.com/320/240?random=#{i+1}"
+  )
+end
