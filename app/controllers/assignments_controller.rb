f914d7eb15edddb7cc948a540cd68ff34e45cd6f
Sebastian Poblete <sebapobletec@gmail.com>
2019-03-27 20:43:54 -0300

    Edit navbar

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,12 +1,12 @@
 class AssignmentsController < ApplicationController
   def create
-      @todos = Todo.all
-      @user = current_user
-      @assignments = []
-      @todos.each do |todo|
-        @user.assignments << Assignment.new(user: current_user, todo: todo)
-      end
-      redirect_to root_path
+    @todos = Todo.all
+    @user = current_user
+    @assignments = []
+    @todos.each do |todo|
+      @user.assignments << Assignment.new(user: current_user, todo: todo)
+    end
+    redirect_to root_path
   end
 
   def change_status
@@ -21,10 +21,13 @@ class AssignmentsController < ApplicationController
   end
 
   def show
-  @assignment = Assignment.find(params[:assignment_id])
-  @todo = @assignment.todo
-  @users = User.joins(:assignments).where(assignments: {completed: true, todo: @todo}).order('name ASC')
-  @ranking = Assignment.joins(:todo).where(assignments: {completed: true, todo: @todo}).order('updated_at ASC').limit(5)
+    @todos = Todo.all
+    @assignments = current_user.assignments
+    @completed_todos = current_user.assignments.where(completed: true).count
 
+    @assignment = Assignment.find(params[:assignment_id])
+    @todo = @assignment.todo
+    @users = User.joins(:assignments).where(assignments: {completed: true, todo: @todo}).order('name ASC')
+    @ranking = Assignment.joins(:todo).where(assignments: {completed: true, todo: @todo}).order('updated_at ASC').limit(5)
   end
 end
