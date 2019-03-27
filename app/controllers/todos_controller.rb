e46fca3a140fdaa6db3cb050a643fe44111923d4
Sebastian Poblete <sebapobletec@gmail.com>
2019-03-26 22:06:34 -0300

    Add user controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,8 @@
 class TodosController < ApplicationController
+before_action :authenticate_user!
+
   def index
-    @todos = Todo.all
+    @user = current_user
+    @assignments = @user.assignments
   end
 end
