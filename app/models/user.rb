3ae24cafb2bb51e72fadda77d71775cb90b6f627
Sebastian Poblete <sebapobletec@gmail.com>
2019-03-28 18:27:28 -0300

    Change loading todos from registration controller to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
   has_many :assignments, dependent: :destroy
   has_many :todos, through: :assignments
+  after_create :load_todos
+
+  private
+
+  def load_todos
+    @todos = Todo.all
+    @user = User.last
+    @assignments = []
+    @todos.each do |todo|
+      @user.assignments << Assignment.new(user: @user, todo: todo)
+    end
+  end
 end
